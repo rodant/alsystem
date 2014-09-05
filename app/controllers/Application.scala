8782ed11bb5e2467c77ef1f17060b105e3cfba73
Antonio Rodriguez <rodant68@gmail.com>
2014-09-05 13:58:56 +0200

    alsystem-2 Login Page (rodant) - login workflow.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,32 @@
 package controllers
 
-import play.api._
+import models.LoginData
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val loginForm: Form[LoginData] = Form(
+    mapping(
+      "userId" -> nonEmptyText,
+      "password" -> nonEmptyText(minLength = 8)
+    )(LoginData.apply)(LoginData.unapply)
+  )
+
+  def home = Action {
+    Ok(views.html.home("Your new application is ready."))
   }
 
   def login = Action {
-    Ok(views.html.login())
+    Ok(views.html.login(loginForm))
+  }
+
+  def doLogin = Action { implicit request =>
+    loginForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.login(formWithErrors)),
+      loginData => Ok(views.html.home(loginData.userId))
+    )
   }
 
 }
---
+++ app/models/LoginData.scala
@@ -0,0 +1,10 @@
+package models
+
+/**
+ * Created by
+ * User: antonio
+ * Date: 05.09.14
+ * Time: 12:16
+ */
+case class LoginData(userId: String, password: String)
+
